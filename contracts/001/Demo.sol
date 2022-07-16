e033d7e14325a2f05cd5ed69d0c50269a0ef0de6
vyeve <xobpaxa@ukr.net>
2022-07-16 11:16:36 +0300

    added Demo contract

---
+++ contracts/001/Demo.sol
@@ -0,0 +1,22 @@
+// SPDX-License-Identifier: MIT
+
+pragma solidity ^0.8.0;
+
+contract Shop {
+    address public owner;
+    mapping(address => uint256) public payments;
+
+    constructor() {
+        owner = msg.sender;
+    }
+
+    function pay() public payable {
+        payments[msg.sender] = msg.value;
+    }
+
+    function withdrawAll() public payable {
+        address payable _to = payable(owner);
+        address _contract = address(this);
+        _to.transfer(_contract.balance);
+    }
+}
